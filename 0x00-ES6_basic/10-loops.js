bc98bf69f53418d59d221e15c41f1ab34eda243d
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-04-24 12:21:21 +0100

    corrected to for of loop at line 4

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   if (array) {
     // eslint-disable-next-line guard-for-in
-    for (const idx in array) {
+    for (const idx of array) {
       const value = array[idx];
       // eslint-disable-next-line no-param-reassign
       array[idx] = appendString + value;
