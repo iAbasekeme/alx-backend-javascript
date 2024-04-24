49ff96c3242aec75a1bd73b6f3326d0944a3d435
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-04-24 13:18:30 +0100

    fixed eslint errors

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [...array];
   for (const idx of array) {
     // eslint-disable-next-line no-param-reassign
     const newValue = appendString + idx;
     const index = array.indexOf(idx);
-    array[index] = newValue;
+    newArray[index] = newValue;
   }
   return array;
 }
