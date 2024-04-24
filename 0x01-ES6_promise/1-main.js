67b4fd7594107f5832ff39f2af6d69b9d9bc3832
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-04-24 20:00:28 +0100

    task 1

---
+++ 0x01-ES6_promise/1-main.js
@@ -0,0 +1,6 @@
+import getFullResponseFromAPI from './1-promise';
+
+// eslint-disable-next-line jest/require-hook
+console.log(getFullResponseFromAPI(true));
+// eslint-disable-next-line jest/require-hook
+console.log(getFullResponseFromAPI(false));
