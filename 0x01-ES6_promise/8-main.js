749a37582069a28bcee92f7f89a600a6a0630b86
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-04-25 15:51:24 +0100

    task 8, throwing errors

---
+++ 0x01-ES6_promise/8-main.js
@@ -0,0 +1,6 @@
+import divideFunction from './8-try';
+
+// eslint-disable-next-line jest/require-hook
+console.log(divideFunction(10, 2));
+// eslint-disable-next-line jest/require-hook
+console.log(divideFunction(10, 0));
