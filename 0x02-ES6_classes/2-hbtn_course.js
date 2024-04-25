558385890f52425c0d7fceffaed6aae79798e15f
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-04-25 20:49:05 +0100

    fixed wrong code

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,16 +1,14 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-underscore-dangle */
 export default class HolbertonCourse {
-  constructor(name, students, length) {
-    this._validateName(name);
-    this._validateLength(length);
-    this._validateStudents(students);
-
-    this._name = name;
-    this._students = students;
-    this._length = length;
+  constructor(name, length, students) {
+    // Type checking and attribute assignment
+    this._name = this._checkString(name, 'name');
+    this._length = this._checkNumber(length, 'length');
+    this._students = this._checkArray(students, 'students');
   }
 
+  // Getters for each attribute
   get name() {
     return this._name;
   }
@@ -20,42 +18,43 @@ export default class HolbertonCourse {
   }
 
   get students() {
-    // Return a copy of the students array to prevent modification
-    return this._students.slice();
+    return [...this._students]; // Return a copy of the students array
   }
 
-  // Setters
+  // Setters for each attribute with type checking
   set name(newName) {
-    this._validateName(newName);
-    this._name = newName;
+    this._name = this._checkString(newName, 'name');
   }
 
   set length(newLength) {
-    this._validateLength(newLength);
-    this._length = newLength;
+    this._length = this._checkNumber(newLength, 'length');
   }
 
   set students(newStudents) {
-    this._validateStudents(newStudents);
-    this._students = newStudents;
+    this._students = this._checkArray(newStudents, 'students');
   }
 
-  // Validation functions (private)
-  _validateName(name) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
+  // Helper function to check if a value is a string and throw an error if not
+  _checkString(value, attributeName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attributeName} must be a string`);
     }
+    return value;
   }
 
-  _validateLength(length) {
-    if (typeof length !== 'number' || Number.isNaN(length)) {
-      throw new TypeError('Length must be a number');
+  // Helper function to check if a value is a number and throw an error if not
+  _checkNumber(value, attributeName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${attributeName} must be a number`);
     }
+    return value;
   }
 
-  _validateStudents(students) {
-    if (!Array.isArray(students) || students.some((student) => typeof student !== 'string')) {
-      throw new TypeError('Students must be an array of strings');
+  // Helper function to check if a value is an array and throw an error if not
+  _checkArray(value, attributeName) {
+    if (!Array.isArray(value)) {
+      throw new TypeError(`${attributeName} must be an array`);
     }
+    return value;
   }
 }
