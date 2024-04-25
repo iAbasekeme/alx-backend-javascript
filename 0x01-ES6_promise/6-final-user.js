f20a87a08dc0d10cfeda25b55a7ae8b0614d9375
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-04-25 12:46:38 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,17 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const [user, photo] = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-    return Promise.allSettled([user, photo]).then((i) => {
-      i.map((item) => ({
-        status: item.status,
-        value: item.status === 'fulfilled' ? item.value : item.reason,
-      }));
-    });
-  } catch (error) {
-    return [
-      { status: 'rejected', value: error },
-    ];
-  }
+  const [user, photo] = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.allSettled([user, photo]).then((i) => {
+    i.map((item) => ({
+      status: item.status,
+      value: item.status === 'fulfilled' ? item.value : item.reason,
+    }));
+  });
 }
