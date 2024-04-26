39b897cb015172abe8f24563d84346db22093151
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-04-26 15:31:56 +0100

    task 6 extending

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,17 @@
+/* eslint-disable lines-between-class-members */
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
