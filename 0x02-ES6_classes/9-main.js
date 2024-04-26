5767748234243bbc19007d3f92fd1771b1c2a9e9
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-04-26 18:27:37 +0100

    task 9

---
+++ 0x02-ES6_classes/9-main.js
@@ -0,0 +1,9 @@
+/* eslint-disable jest/require-hook */
+import listOfStudents from './9-hoisting';
+
+console.log(listOfStudents);
+
+const listPrinted = listOfStudents.map(
+  (student) => student.fullStudentDescription,
+);
+console.log(listPrinted);
