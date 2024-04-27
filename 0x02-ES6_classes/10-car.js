0bce5a7a62a40a050fafcf73373dd5be3d4be095
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-04-27 09:37:07 +0100

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -2,20 +2,12 @@
 /* eslint-disable no-underscore-dangle */
 export default class Car {
   constructor(brand, motor, color) {
-    this._brand = this._checkString(brand, 'brand');
-    this._motor = this._checkString(motor, 'motor');
-    this._color = this._checkString(color, 'color');
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
     return new this.constructor(this._brand, this._motor, this._color);
-  }
-
-  _checkString(name, attr) {
-    if (typeof name !== 'string') {
-      throw new TypeError(`${attr} must be a string`);
-    } else {
-      return name;
-    }
   }
 }
