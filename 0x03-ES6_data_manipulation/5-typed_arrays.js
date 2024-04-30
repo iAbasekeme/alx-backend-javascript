39e3564e59863fd28238151e49235c22b400cef0
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-04-30 13:59:46 +0100

    fixed error on task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -7,5 +7,8 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8View = new Int8Array(buffer);
   int8View[position] = value;
-  return buffer;
+
+  const dataView = new DataView(buffer);
+
+  return dataView;
 }
