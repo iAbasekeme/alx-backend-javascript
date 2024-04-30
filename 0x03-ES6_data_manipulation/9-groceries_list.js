717eb82346738b5242e016ed70fc9bdaa6cb78ec
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-04-30 14:43:09 +0100

    task 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,4 @@
+export default function groceriesList() {
+  const newMap = new Map([['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]]);
+  return newMap;
+}
---
+++ 0x03-ES6_data_manipulation/9-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable quotes */
+/* eslint-disable import/extensions */
+/* eslint-disable jest/require-hook */
+import groceriesList from "./9-groceries_list.js";
+
+console.log(groceriesList());
