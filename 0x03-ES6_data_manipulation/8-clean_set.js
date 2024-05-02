c2dff7de106bc4c481dabb5e6b8dfdbf8bfefaed
iAbasekeme <109957088+iAbasekeme@users.noreply.github.com>
2024-05-02 01:28:35 +0100

    Delete 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++
@@ -1,7 +0,0 @@
-export default function cleanSet(set, startString) {
-  const filteredValues = Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length));
-
-  return filteredValues.join('-');
-}
